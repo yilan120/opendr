cf8cf185c0cdcaa46aedb6d288f1928322f10a64
Yilan Dong <acw773@frontend8.apocrita>
2024-01-14 22:42:11 +0000

    fi bug

--- opendr/renderer.py
+++ opendr/renderer.py
@@ -20,12 +20,12 @@ from .common import draw_visibility_image, draw_barycentric_image, draw_colored_
 from .topology import get_vertices_per_edge, get_faces_per_edge
 
 if platform.system()=='Darwin':
-    from .contexts.ctx_mac import OsContext
+    from opendr.contexts.ctx_mac import OsContext
 else:
-    from .contexts.ctx_mesa import OsContext
+    from opendr.contexts.ctx_mesa import OsContext
 
 from chumpy import *
-from .contexts._constants import *
+from opendr.contexts._constants import *
 from chumpy.utils import row, col
 
 
